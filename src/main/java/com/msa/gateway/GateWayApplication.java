e479b1d0bf8d39c1b87eda13ec74a41dd6ba248a
user <user@LAPTOP-U0CO45L6>
2023-09-22 17:08:24 +0900

    init

---
+++ src/main/java/com/msa/gateway/GateWayApplication.java
@@ -0,0 +1,14 @@
+package com.msa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
